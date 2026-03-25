eaebc7fb746bf3e1738a89a7bde44ee320d48618
Sezer Ünalmış <76732537+Sezer5@users.noreply.github.com>
2026-03-25 11:39:14 +0300

    sf

--- resources/views/admin/partialviews/sidebar.blade.php
+++ resources/views/admin/partialviews/sidebar.blade.php
@@ -1,7 +1,7 @@
 <aside class="sidebar">
             <nav>
                 <ul>
-                    <li><a href="#">Dashboard</a></li>
+                    <li><a href="{{route('admin.home')}}"><i class="fas fa-home"></i> Anasayfa</a></li>
                     <li><a href="#">Kullanıcılar</a></li>
                     <li><a href="#">Ürünler</a></li>
                     <li><a href="#">Ayarlar</a></li>
