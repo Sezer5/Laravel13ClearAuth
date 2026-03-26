d9da4051df7e41466bf6f191140455b09af4042d
Sezer Ünalmış <76732537+Sezer5@users.noreply.github.com>
2026-03-26 11:50:54 +0300

    sf

--- resources/views/admin/partialviews/header.blade.php
+++ resources/views/admin/partialviews/header.blade.php
@@ -1,7 +1,7 @@
 <header class="header">
     <h2><i class="fa-solid fa-user-tie"></i> Easy Admin</h2>
     <div class="user-dropdown">
-        <button class="dropbtn">Hoş geldin, {{Auth::user()->name}} ▼</button>
+        <button class="dropbtn">Hoş geldin, <b>{{Auth::user()->name}} ▼</b></button>
         <div class="dropdown-content">
             <a href="#">Profilim</a>
             <a href="#">Ayarlar</a>
